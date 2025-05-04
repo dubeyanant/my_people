e58da6db224d03ececc9c48ecbcf23399cf166cd
anant <anantkumar312@gmail.com>
2025-05-04 14:01:20 +0530

    refactor: home screen

--- lib/modules/home/home_screen.dart
+++ lib/modules/home/home_screen.dart
@@ -9,73 +9,124 @@ import 'package:my_people/modules/home/widgets/empty_home.dart';
 import 'package:my_people/modules/home/widgets/people_grid.dart';
 import 'package:my_people/utility/constants.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final PeopleController pc = Get.put(PeopleController());
-    final TextEditingController searchController = TextEditingController();
-    final FocusNode searchFocusNode = FocusNode();
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
-    return Obx(
-      () => Scaffold(
-        appBar: AppBar(
-          title: const Text(AppStrings.appName),
-          actions: [
-            if (pc.people.isNotEmpty)
-              IconButton(
-                onPressed: () {
-                  pc.isSearchOpen.value = !pc.isSearchOpen.value;
-                  if (!pc.isSearchOpen.value) {
-                    searchController.clear();
-                    pc.filterPeople('');
-                    searchFocusNode.unfocus();
-                  }
-                },
-                icon: Icon(
-                  pc.isSearchOpen.value ? Icons.cancel_outlined : Icons.search,
-                ),
-              ),
-          ],
+class _HomeScreenState extends State<HomeScreen> {
+  late final PeopleController _peopleController;
+  final TextEditingController _searchController = TextEditingController();
+  final FocusNode _searchFocusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    // If PeopleController is not initialized yet
+    if (!Get.isRegistered<PeopleController>()) {
+      Get.put(PeopleController());
+    }
+    _peopleController = Get.find<PeopleController>();
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    _searchFocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: Obx(() => _buildBody()),
+      floatingActionButton: _buildFloatingActionButton(),
+    );
+  }
+
+  PreferredSizeWidget _buildAppBar() {
+    return AppBar(
+      title: const Text(AppStrings.appName),
+      actions: [
+        Obx(() {
+          if (_peopleController.people.isEmpty) return const SizedBox();
+
+          return IconButton(
+            onPressed: _toggleSearch,
+            icon: Icon(
+              _peopleController.isSearchOpen.value
+                  ? Icons.cancel_outlined
+                  : Icons.search,
+            ),
+          );
+        }),
+      ],
+    );
+  }
+
+  void _toggleSearch() {
+    _peopleController.isSearchOpen.value =
+        !_peopleController.isSearchOpen.value;
+    if (!_peopleController.isSearchOpen.value) {
+      _searchController.clear();
+      _peopleController.filterPeople('');
+      _searchFocusNode.unfocus();
+    }
+  }
+
+  Widget _buildBody() {
+    if (_peopleController.people.isEmpty) {
+      return const EmptyHome();
+    }
+
+    return Column(
+      children: [
+        _buildSearchBar(),
+        const Expanded(child: PeopleGrid()),
+      ],
+    );
+  }
+
+  Widget _buildSearchBar() {
+    if (!_peopleController.isSearchOpen.value) {
+      return const SizedBox.shrink();
+    }
+
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+      child: TextField(
+        focusNode: _searchFocusNode,
+        autofocus: true,
+        controller: _searchController,
+        decoration: InputDecoration(
+          hintText: AppStrings.searchBarHintText,
+          prefixIcon: const Icon(Icons.search),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
         ),
-        body: pc.people.isEmpty
-            ? const EmptyHome()
-            : Column(
-                children: [
-                  if (pc.isSearchOpen.value)
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
-                      child: TextField(
-                        focusNode: searchFocusNode,
-                        autofocus: true,
-                        controller: searchController,
-                        decoration: InputDecoration(
-                          hintText: AppStrings.searchBarHintText,
-                          prefixIcon: const Icon(Icons.search),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                        onChanged: (value) {
-                          pc.filterPeople(value);
-                        },
-                      ),
-                    ),
-                  const Expanded(child: PeopleGrid()),
-                ],
-              ),
-        floatingActionButton: pc.people.isEmpty
-            ? null
-            : AnimatedPressButton(
-                onPressed: () => showPersonDetailBottomSheet(context),
-                child: Icon(
-                  Icons.add,
-                  size: 28,
-                  color: Theme.of(context).colorScheme.onPrimary,
-                ),
-              ),
+        onChanged: _peopleController.filterPeople,
       ),
     );
+  }
+
+  Widget _buildFloatingActionButton() {
+    return Obx(() {
+      if (_peopleController.people.isEmpty) {
+        return const SizedBox.shrink();
+      }
+
+      return AnimatedPressButton(
+        onPressed: () => showPersonDetailBottomSheet(context),
+        child: Icon(
+          Icons.add,
+          size: 28,
+          color: Theme.of(context).colorScheme.onPrimary,
+        ),
+      );
+    });
   }
 }
