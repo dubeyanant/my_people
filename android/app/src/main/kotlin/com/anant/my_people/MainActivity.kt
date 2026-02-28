140188d36bbaa214c0494da1660e7bcc7a1ae000
anant <anantkumar312@gmail.com>
2026-02-28 19:31:34 +0530

    chore: made 16kb compatible

--- android/app/src/main/kotlin/com/anant/my_people/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.infiniteants.mypeople
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/example/my_people/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.infiniteants.mypeople
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
